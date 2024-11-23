5b86b258d613eccd8ca5a745a565550d7fdc385d
selcher_k <selcher@juno.com>
2024-11-23 03:35:30 -0500

    Copied accelerate-theme-child files

--- front-page.php
+++ front-page.php
@@ -12,31 +12,31 @@
  * @since Accelerate Marketing 2.0
  */
 
- get_header(); ?>
+get_header(); ?>
 
- <div id="primary" class="home-page hero-content">
-	 <div class="main-content" role="main">
-		 <?php while ( have_posts() ) : the_post(); ?>
-			 <?php the_content(); ?>
-				 <a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
-		 <?php endwhile; // end of the loop. ?>
-	 </div><!-- .main-content -->
- </div><!-- #primary -->
+	<div id="primary" class="home-page hero-content">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+					<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+	</div><!-- #primary -->
 
- <!-- RECENT BLOG POST -->
- <section class="recent-posts">
-	 <div class="site-content">
-		 <div class="blog-post">
-			 <h4>From the Blog</h4>
-			 <?php query_posts('posts_per_page=1'); ?>
-				 <!-- the loop -->
-				 <?php while ( have_posts() ) : the_post(); ?>
-					 <h3><?php the_title(); ?></h3>
-					 <?php the_excerpt(); ?>
-				 <?php endwhile; // end of the loop. ?>
-			 <?php wp_reset_query(); // resets the altered query back to the original ?>
-		 </div>
-	 </div>
- </section>
+	<!-- RECENT BLOG POST -->
+	<section class="recent-posts">
+		<div class="site-content">
+			<div class="blog-post">
+				<h4>From the Blog</h4>
+				<?php query_posts('posts_per_page=1'); ?>
+					<!-- the loop -->
+					<?php while ( have_posts() ) : the_post(); ?>
+						<h3><?php the_title(); ?></h3>
+						<?php the_excerpt(); ?>
+					<?php endwhile; // end of the loop. ?>
+				<?php wp_reset_query(); // resets the altered query back to the original ?>
+			</div>
+		</div>
+	</section>
 
 <?php get_footer(); ?>
