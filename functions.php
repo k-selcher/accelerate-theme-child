a237928b1336442671561a203b12181ba4c8e727
selcher_k <selcher@juno.com>
2024-12-11 00:04:03 -0500

    Initial commit

--- functions.php
+++ functions.php
@@ -34,4 +34,3 @@ function create_custom_post_types() {
     );
 }
 add_action( 'init', 'create_custom_post_types' );
-
---
+++ mailchimp.php
@@ -0,0 +1,3 @@
+
+<p>This is a project I made for this company for this. Here is the early-stage startup that I helped design and do front-end and back-end dev for.</p>
+<p class="read-more-link"><a href="#">Visit Live Site &rsaquo;</a></p>
